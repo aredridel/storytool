36349a502ab639fafc3f14d7f81662b74c8b7a42
Aria Stewart <aredridel@dinhe.net>
2019-03-22 18:52:03 -0400

    Begin moving to vfile as an abstraction

--- storyweb/load.js
+++ storyweb/load.js
@@ -4,9 +4,11 @@ const { promisify } = require('util');
 const readFile = promisify(require('fs').readFile)
 const url = require('url')
 
+const vfile = require('to-vfile')
+
 module.exports = async function (u) {
 	const path = url.fileURLToPath(u)
-	const file = await readFile(path, 'utf-8')
+	const file = await vfile.read(path, 'utf-8')
     const ast = parser.parse(file)
     return { type: 'file', url: u, ast }
 }

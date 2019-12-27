2bd44ef6d8971af29afae5ed731494e022f98d2d
Aria Stewart <aredridel@dinhe.net>
2019-12-26 21:07:50 -0500

    Integrate storymap

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 import yargs from "yargs";
 import idCmd from "./commands/id.js";
 import sceneCmd from "./commands/scene.js";
+import mapCmd from "./commands/map.js";
 
 const argv = yargs
 	.usage('$0 <cmd> [args]')
 	.command(idCmd)
 	.command(sceneCmd)
+	.command(mapCmd)
 	.demandCommand()
 	.help()
 	.strict()

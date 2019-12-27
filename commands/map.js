a9bef162ea95d26089dc4c8538a5f3354bd0e07d
Aria Stewart <aredridel@dinhe.net>
2019-12-26 21:08:58 -0500

    Actually include the file

---
+++ commands/map.js
@@ -0,0 +1,27 @@
+import { WikiMap, mapToDot } from "storymap";
+// File handling
+import url from 'url';
+
+export default {
+	command : "map <command>",
+	desc : "Story Map Commands",
+	builder: yargs => {
+		yargs.command({
+			command: 'generate <files...>',
+			desc: "Generate Story Map",
+			handler: async argv => {
+				console.log(argv);
+				try {
+					const st = argv.files.map(x => url.pathToFileURL(x));
+					const map = new WikiMap();
+					const loaded = await map.run(st)
+					console.log(await mapToDot(url.pathToFileURL('.'), loaded))
+				} catch (e) {
+					console.warn(e);
+					process.exit(1);
+				}
+			}
+		});
+	},
+	handler : (argv) => {}
+};
